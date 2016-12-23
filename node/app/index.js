ab4ed36ee0bf02d8b6b4ec2427c6045cc065f969
Dmitry Bezheckov <dima00782@gmail.com>
2016-12-24 04:43:47 +0700

    add Map

--- node/app/index.js
+++ node/app/index.js
@@ -7,6 +7,27 @@ wss.on('open', function() {
 	// nothing to do here
 });
 
+function Map(col, func, blockSize = 5) {
+	let nblocks = (Math.floor(col.length / blockSize) + (col.length % blockSize == 0 ? 0 : 1));
+	for (let i = 0; i < nblocks; ++i) {
+		let size = col.length - (i + 1) * blockSize > 0 ? blockSize : col.length - i * blockSize;
+		console.log(size);
+
+		let arguments = col.slice().splice(i * blockSize, size);
+		arguments = arguments.join('\n');
+
+		let functionText = "function map_iter(a)"
+			+ "{ console.log(a); for (let i = " + i * blockSize + "; i < " + (i * blockSize + size) + "; ++i) {"
+			+ "(" + func.toString() + ")(a); }}";
+
+		let message = JSON.stringify({
+			functionText : functionText,
+			functionArgumentsText : arguments
+		});
+		wss.send(message);
+	}
+}
+
 wss.on('message', function(data, flags) {
 	let obj = null;
 	try {
@@ -22,6 +43,8 @@ wss.on('message', function(data, flags) {
 
 	let fn = eval("() => { return " + functionText + ";}")();
 	let packagedFn = (...args) => fn(...functionArguments);
+
+	console.log("Run:", functionText, "Arguments:", functionArguments);
 
 	packagedFn();
 });
